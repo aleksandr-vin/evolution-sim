311c3d05e7de4f1c0ffd58f93c899c739f9c1bc0
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2023-07-21 22:33:11 +0200

    add saving of the field to PNG

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use rand::prelude::*;
 
+// Run parameters
+const X: usize = 256;
+const Y: usize = 256;
+const SPECIES: usize = 10000;
+
+// Imaging options
+const IMG_SCALE_X: usize = 5;
+const IMG_SCALE_Y: usize = 5;
+
+// Implementation constants
+const FIELD_SIZE: usize = X * Y;
+
+///////////////////////////////////////
+
 struct Cell {
     taken: bool,
 }
-
-const X: usize = 7;
-const Y: usize = 5;
-const SPECIES: usize = 4;
-
-const FIELD_SIZE: usize = X * Y;
 
 fn main() {
     let mut field = [&Cell { taken: false }; FIELD_SIZE];
@@ -17,6 +25,7 @@ fn main() {
 
     take_cells(&mut field, &mut rng);
     display_field(&field);
+    save_image_field(&field);
 }
 
 fn take_cells(field: &mut [&Cell; FIELD_SIZE], rng: &mut ThreadRng) {
@@ -25,9 +34,7 @@ fn take_cells(field: &mut [&Cell; FIELD_SIZE], rng: &mut ThreadRng) {
     let taken_cells = Vec::from(taken_cells);
 
     for c in taken_cells {
-        field[c] = &Cell {
-            taken: true,
-        }
+        field[c] = &Cell { taken: true }
     }
 }
 
@@ -40,3 +47,21 @@ fn display_field(field: &[&Cell; FIELD_SIZE]) {
     }
 }
 
+fn save_image_field(field: &[&Cell; FIELD_SIZE]) {
+    let mut imgbuf = image::ImageBuffer::new(
+        (X * IMG_SCALE_X).try_into().unwrap(),
+        (Y * IMG_SCALE_Y).try_into().unwrap(),
+    );
+
+    for (px, py, pixel) in imgbuf.enumerate_pixels_mut() {
+        let x: usize = (px / IMG_SCALE_X as u32).try_into().unwrap();
+        let y: usize = (py / IMG_SCALE_Y as u32).try_into().unwrap();
+        let taken = field[y * X + x].taken;
+        let r = if taken { 0 } else { 250 } as u8;
+        let g = if taken { 0 } else { 250 } as u8;
+        let b = if taken { 0 } else { 250 } as u8;
+        *pixel = image::Rgb([r, g, b]);
+    }
+
+    imgbuf.save("field.png").unwrap();
+}
