4a9a4d0640743da8918fab7ce196423f32802488
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2023-08-03 19:07:26 +0200

    refactor some names

--- src/main.rs
+++ src/main.rs
@@ -14,40 +14,40 @@ const FIELD_SIZE: usize = X * Y;
 
 ///////////////////////////////////////
 
-struct Cell {
-    taken: bool,
+struct Tile {
+    occupied: bool,
 }
 
 fn main() {
-    let mut field = [&Cell { taken: false }; FIELD_SIZE];
+    let mut field = [&Tile { occupied: false }; FIELD_SIZE];
 
     let mut rng = rand::thread_rng();
 
-    take_cells(&mut field, &mut rng);
+    randmomly_spread_cells_over_tiles(&mut field, &mut rng);
     display_field(&field);
     save_image_field(&field);
 }
 
-fn take_cells(field: &mut [&Cell; FIELD_SIZE], rng: &mut ThreadRng) {
-    let mut taken_cells: Vec<usize> = (0..FIELD_SIZE).collect();
-    let (taken_cells, _) = taken_cells.partial_shuffle(rng, SPECIES);
-    let taken_cells = Vec::from(taken_cells);
+fn randmomly_spread_cells_over_tiles(field: &mut [&Tile; FIELD_SIZE], rng: &mut ThreadRng) {
+    let mut indexes: Vec<usize> = (0..FIELD_SIZE).collect();
+    let (indexes, _) = indexes.partial_shuffle(rng, SPECIES);
+    let indexes = Vec::from(indexes);
 
-    for c in taken_cells {
-        field[c] = &Cell { taken: true }
+    for c in indexes {
+        field[c] =  &Tile{ occupied: true }
     }
 }
 
-fn display_field(field: &[&Cell; FIELD_SIZE]) {
+fn display_field(field: &[&Tile; FIELD_SIZE]) {
     for y in 0..Y {
         for x in 0..X {
-            print!(" {}", if field[y * X + x].taken { '+' } else { 'O' });
+            print!(" {}", if field[y * X + x].occupied { '+' } else { 'O' });
         }
         println!("");
     }
 }
 
-fn save_image_field(field: &[&Cell; FIELD_SIZE]) {
+fn save_image_field(field: &[&Tile; FIELD_SIZE]) {
     let mut imgbuf = image::ImageBuffer::new(
         (X * IMG_SCALE_X).try_into().unwrap(),
         (Y * IMG_SCALE_Y).try_into().unwrap(),
@@ -56,7 +56,7 @@ fn save_image_field(field: &[&Cell; FIELD_SIZE]) {
     for (px, py, pixel) in imgbuf.enumerate_pixels_mut() {
         let x: usize = (px / IMG_SCALE_X as u32).try_into().unwrap();
         let y: usize = (py / IMG_SCALE_Y as u32).try_into().unwrap();
-        let taken = field[y * X + x].taken;
+        let taken = field[y * X + x].occupied;
         let r = if taken { 0 } else { 250 } as u8;
         let g = if taken { 0 } else { 250 } as u8;
         let b = if taken { 0 } else { 250 } as u8;
